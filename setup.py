c1abd21d77cc256335353bb96b306f7b8d94e0db
ForgettenAccount <98701076+ForgettenAccount@users.noreply.github.com>
2022-09-06 08:36:08 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="stdlib",
+      version="2.0.1",
+      description="A worse module ever made..",
+      author="DuckTech",
+      packages=["stdlib"],
+      requires=['pycolor'],
+      zip_safe=False)
