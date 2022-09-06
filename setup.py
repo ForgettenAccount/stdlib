36111062bef9325ef9fa87baf7c9c0b35848ef90
ForgettenAccount <98701076+ForgettenAccount@users.noreply.github.com>
2022-09-06 08:51:12 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup
 setup(name="stdlib",
       version="2.0.1",
       description="A worse module ever made..",
-      home_page="https://github.com/ForgettenAccount/stdlib/"
+      home_page="https://github.com/ForgettenAccount/stdlib/",
+      license="Apache 2.0",
       author="ForgettenAccout",
       packages=["stdlib"],
       requires=['pycolor'],
